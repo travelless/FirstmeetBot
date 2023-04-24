a56dfb3fd314b58eb3fe40cba423e4a1c7a2a172
firstmeet1108 <2022742378@qq.com>
2023-04-24 20:09:51 +0800

    天气预报部分 1.0 准备接入hdu接口获取课表信息

--- external/special/src/index.ts
+++ external/special/src/index.ts
@@ -1,7 +1,7 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Service, h } from 'koishi'
 import cron from 'koishi-plugin-cron'
-import { } from '@koishijs/plugin-adapter-onebot'
-
+import {} from '@koishijs/plugin-adapter-onebot'
+import { weatherQuery } from 'koishi-plugin-weather'
 export const name = 'special'
 
 export interface Config {}
@@ -9,12 +9,83 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // let groupList = await database.getGroupList()
-  
-  
-  ctx.cron('0 1 1 * * *', async () => {
-    console.log(ctx.bots[0]);
-    // console.log(await ctx.bots[0].getFriendList());
+  let special_id = process.env.SPECIAL_ID
+  ctx.middleware(async (session, next) => {
+    if(session.userId == "2022742378") {
+      let weather = (await weatherQuery("杭州")).forecasts[0]
+      let time = getTime(weather.casts[0].week)
+      let message = `<>   
+                  <p> xx同学 早安  </p>
+                  <p>时间： ${time} </p> 
+                  <p>地区： ${weather.city}</p> 
+                  <p>今日天气： </p>
+                  <p>          白天-${weather.casts[0].dayweather}  晚上-${weather.casts[0].nightweather}</p>
+                  <p>          温度 ${weather.casts[0].nighttemp}-${weather.casts[0].daytemp} ℃</p>
+                  <p>          风力 ${weather.casts[0].daywind} 级</p>
+                  <p>明日天气：</p>
+                  <p>          白天-${weather.casts[1].dayweather}  晚上-${weather.casts[1].nightweather}</p>
+                  <p>          温度 ${weather.casts[1].nighttemp}-${weather.casts[1].daytemp} ℃</p>
+                  <p>          风力 ${weather.casts[1].daywind} 级</p> 
+                  </>`
+      ctx.bots[0].sendPrivateMessage("2022742378", message)
+      await next()
+    }
   })
 
+  
+  ctx.cron('0 30 7 * * *', async () => {
+    let weather = (await weatherQuery("杭州")).forecasts[0]
+    let time = getTime(weather.casts[0].week)
+    let message = `<>   
+                  <p> xx同学 早安  </p>
+                  <p>时间： ${time} </p> 
+                  <p>地区： ${weather.city}</p> 
+                  <p>今日天气： </p>
+                  <p>          白天-${weather.casts[0].dayweather}  晚上-${weather.casts[0].nightweather}</p>
+                  <p>          温度 ${weather.casts[0].nighttemp}-${weather.casts[0].daytemp} ℃</p>
+                  <p>          风力 ${weather.casts[0].daywind} 级</p>
+                  <p>明日天气：</p>
+                  <p>          白天-${weather.casts[1].dayweather}  晚上-${weather.casts[1].nightweather}</p>
+                  <p>          温度 ${weather.casts[1].nighttemp}-${weather.casts[1].daytemp} ℃</p>
+                  <p>          风力 ${weather.casts[1].daywind} 级</p> 
+                  </>`
+    ctx.bots[0].sendPrivateMessage(process.env.SPECIAL_ID, message)
+  })
+
+}
+
+
+function getTime(week){
+  let weekday
+  switch (week) {
+    case '1':
+      weekday = '星期一'
+      break;
+    case '2':
+      weekday = '星期二'
+      break;
+    case '3':
+      weekday = '星期三'
+      break;
+    case '4':
+      weekday = '星期四'
+      break;
+    case '5':
+      weekday = '星期五'
+      break;
+    case '6':
+      weekday = '星期六'
+      break;
+    case '7':
+      weekday = '星期日'
+      break;
+    default:
+      break;
+  }
+  let date = new Date()
+  let year = date.getFullYear()
+  let month = date.getMonth() + 1
+  let day = date.getDate()
+
+  return `${year}-${month}-${day} ${weekday} `
 }
--- external/weather/src/index.ts
+++ external/weather/src/index.ts
@@ -1,7 +1,6 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Service } from 'koishi'
 import * as fs from 'fs'
 import axios from 'axios'
-import { getWeather } from './getWeather'
 
 export const name = 'weather'
 
@@ -12,17 +11,42 @@ export const Config: Schema<Config> = Schema.object({})
 export function apply(ctx: Context) {
   ctx.command('weather <city>', 'Get weather information of a city')
     .action(async ({ session }, city) => {
+      // console.log(session.);
       if(!city){
         city = '杭州'
       }
-      console.log(process.env.WEATHER_QUERY_KEY);
-      let weather = await getWeather(city)
-      
+      let weather = await weatherQuery(city)
       return `<>
-      你好，同学
       今天${city}的天气是${weather.lives[0].weather}，
       温度是${weather.lives[0].temperature}摄氏度，
-      风力是${weather.lives[0].windpower}级，
+      风力${weather.lives[0].windpower}级，
     </>`
     })
+}
+
+export async function weatherQuery(city:string) {
+  let params = {
+    key: process.env.WEATHER_QUERY_KEY,
+    city: "330100",
+    extensions: 'all',
+    output: 'json',
+  }
+  let cityCodeList = JSON.parse(fs.readFileSync('./public/citycode.json', 'utf-8'))
+  for(let i of cityCodeList) {
+    if(i.name == city) {
+      params.city = i.adcode
+    }
+  }
+  try {
+    let response: any = await axios({
+      url: 'https://restapi.amap.com/v3/weather/weatherInfo',
+      method: "GET",
+      params: params,
+    })
+    response = await response.data
+    return response
+  } catch (e) {
+    console.log('error', e)
+    return e
+  }
 }
