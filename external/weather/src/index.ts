57ded7351d321974fedea8b71cba6f38e5d80e71
firstmeet1108 <2022742378@qq.com>
2023-04-24 20:46:32 +0800

    修改了天气查询

--- external/weather/src/index.ts
+++ external/weather/src/index.ts
@@ -12,30 +12,52 @@ export function apply(ctx: Context) {
   ctx.command('weather <city>', 'Get weather information of a city')
     .action(async ({ session }, city) => {
       // console.log(session.);
-      if(!city){
-        city = '杭州'
+      if(city === undefined){
+        city = "杭州"
       }
       let weather = await weatherQuery(city)
-      return `<>
-      今天${city}的天气是${weather.lives[0].weather}，
-      温度是${weather.lives[0].temperature}摄氏度，
-      风力${weather.lives[0].windpower}级，
-    </>`
+      if(weather == "城市名错误"){
+        console.log("城市名错误!!!!");
+        return "城市名错误"
+      }else{
+        weather = weather.forecasts[0]
+      }
+      // console.log(weather);
+      let message = `<>   
+                    <p>时间： ${weather.reporttime} </p> 
+                    <p>地区： ${weather.city}</p> 
+                    <p>今日天气： </p>
+                    <p>          白天-${weather.casts[0].dayweather}  晚上-${weather.casts[0].nightweather}</p>
+                    <p>          温度 ${weather.casts[0].nighttemp}-${weather.casts[0].daytemp} ℃</p>
+                    <p>          风力 ${weather.casts[0].daywind} 级</p>
+                    <p>明日天气：</p>
+                    <p>          白天-${weather.casts[1].dayweather}  晚上-${weather.casts[1].nightweather}</p>
+                    <p>          温度 ${weather.casts[1].nighttemp}-${weather.casts[1].daytemp} ℃</p>
+                    <p>          风力 ${weather.casts[1].daywind} 级</p> 
+                    </>`
+      return message
     })
 }
 
 export async function weatherQuery(city:string) {
+  console.log(city);
   let params = {
     key: process.env.WEATHER_QUERY_KEY,
     city: "330100",
     extensions: 'all',
     output: 'json',
   }
+  let idx = 0
   let cityCodeList = JSON.parse(fs.readFileSync('./public/citycode.json', 'utf-8'))
   for(let i of cityCodeList) {
-    if(i.name == city) {
+    if( i.name.search(city) != -1 ) {
       params.city = i.adcode
+      idx = 1
+      break;
     }
+  }
+  if(idx == 0) {
+    return "城市名错误"
   }
   try {
     let response: any = await axios({
